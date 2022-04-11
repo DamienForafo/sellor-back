b13acfc4bcf6e6f8081c8266ec2c587102faaa81
DamienForafo <damien.forafo@gmail.com>
2022-04-11 17:21:24 +0200

    Working CRUD on User bis

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 const {registerValidate} = require('../validation');
 
 
