854d5f9b2998207a81f7ff4c63f196785d978b30
AyaHaddad <aya.haddad@ynov.com>
2022-04-25 18:43:18 +0200

    fix route login + add route logged user

--- index.js
+++ index.js
@@ -1,44 +1,39 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser'); // parse body requests before handling it
-const cors = require('cors'); // restrict which domains can access
-const {verifyToken} = require('./authChecks');
-const User = require('./models/User');
-const Product = require('./models/Product');
-
-
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser"); // parse body requests before handling it
+const cors = require("cors"); // restrict which domains can access
+const { verifyToken } = require("./authChecks");
+const User = require("./models/User");
+const Product = require("./models/Product");
 
 // Import routes
-const authRoute = require('./routes/auth');
-const usersRoute = require('./routes/users');
-const productsRoute = require('./routes/products');
-
+const authRoute = require("./routes/auth");
+const usersRoute = require("./routes/users");
+const productsRoute = require("./routes/products");
 
 // Read .env and put it in process.env
 dotenv.config();
 
-
 // Connect to DB
-mongoose.connect(
-    process.env.DB_CONNECT,
-    { useNewUrlParser: true },
-    () => console.log('Connected to DB !')
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () =>
+  console.log("Connected to DB !")
 );
-
 
 // Middlewares
 app.use(bodyParser.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
-
 // Routes middlewares
-app.use('/auth', authRoute);
-app.use('*', verifyToken);
-app.use('/users', usersRoute);
-app.use('/products', productsRoute);
+app.use("/auth", authRoute);
+app.use("*", verifyToken);
+app.use("/users", usersRoute);
+app.use("/products", productsRoute);
 
-
-app.listen(3000, () => console.log('Server up and running !'));
+app.listen(3000, () => console.log("Server up and running !"));
